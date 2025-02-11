38e0a3b2442c9d00332a48978146396d6c7dfcb1
Sheikh Jabed <jabedbd2295@gmail.com>
2025-02-11 21:23:29 +0600

    Index JS Updated

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
