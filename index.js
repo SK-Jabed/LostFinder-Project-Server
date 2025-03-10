5b1482ae7936b78b177e277e4c11a039d906728a
Sheikh Jabed <jabedbd2295@gmail.com>
2025-03-10 23:40:42 +0600

    New CORS Server Added and Credentials Added

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // Middleware
 const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://localhost:5174",
+    "http://localhost:5175",
     "https://b10-assignment-11-753d2.web.app",
     "https://b10-assignment-11-753d2.firebaseapp.com",
   ],
