ce371fe0a34ab803c3a408c0b069329497ad3567
Sheikh Jabed <jabedbd2295@gmail.com>
2025-04-25 19:31:46 +0600

    [Updated]: New Functionalities and Activities Updated of Server Side

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 6000;
 
 // Middleware
 const corsOptions = {
